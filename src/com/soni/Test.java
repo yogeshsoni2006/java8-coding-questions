2d25d40c9d9e94796c104b98b237603b8952bd5c
yogeshsoni2006 <yogeshsoni2006@gmail.com>
2024-11-23 14:17:03 +0530

    add new project

---
+++ src/com/soni/Test.java
@@ -0,0 +1,27 @@
+package com.soni;
+
+import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+import static java.util.List.of;
+import static java.util.stream.Collectors.counting;
+import static java.util.stream.Collectors.groupingBy;
+
+public class Test {
+    public static void main(String args[]) {
+        List<Integer> oneToTen = of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+        List<Integer> al = of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10,2,10);
+        List<Integer> removeDup = al.stream().distinct().collect(Collectors.toList());
+        System.out.println(removeDup);
+        Set<Integer> removeDup1 = new HashSet<>(al);
+        System.out.println(removeDup1);
+
+        List<Integer> even = oneToTen.stream().filter(i->i%2==0).collect(Collectors.toList());
+        System.out.println(even);
+
+        String name = "yogesh yog";
+        Map<String, Long> al1 = Arrays.stream(name.split("")).collect(groupingBy(Function.identity(), counting()));
+        System.out.println(al1);
+    }
+}
