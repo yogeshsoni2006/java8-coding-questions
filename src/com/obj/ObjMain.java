50a02ee5c34401176c18f48ac91448e83cab6888
yogeshsoni2006 <yogeshsoni2006@gmail.com>
2024-12-01 22:00:06 +0530

    added employee codes

--- src/com/obj/ObjMain.java
+++ src/com/obj/ObjMain.java
@@ -22,7 +22,11 @@ public class ObjMain {
         Employee highestPaid = employees.stream()
                 .max(Comparator.comparingDouble(Employee::getSalary))
                 .orElse(null);
-        System.out.println(highestPaid);
+        //System.out.println(highestPaid);
+        Employee highestPaid1 = employees.stream()
+                .max(Comparator.comparing(Employee::getSalary))
+                .orElse(null);
+        System.out.println(highestPaid1);
 
         //Sort Employee by name
         System.out.println("\nSort Employees by name:");
